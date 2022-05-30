5da9830de6cdbc3b5256334c5340637d38871fee
nGabe <novac.gabi99@gmail.com>
2022-05-30 20:04:22 +0300

    firstCommit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="">.getCar</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
